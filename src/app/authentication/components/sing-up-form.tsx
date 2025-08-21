63711cc3bf8c92297bcc8919f0d28e7d387d150c
NathanManhkeBNUSC <nathanmanhke@gmail.com>
2025-08-21 14:59:18 -0300

    feat: add custom error messagem for existing user

--- src/app/authentication/components/sing-up-form.tsx
+++ src/app/authentication/components/sing-up-form.tsx
@@ -24,6 +24,7 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 const registerSchema = z.object({
   name: z.string().min(1, { message: "Nome é obrigatório" }),
@@ -59,6 +60,13 @@ const SingUpForm = () => {
       {
         onSuccess: () => {
           router.push("/dashboard");
+        },
+        onError: (ctx) => {
+          if (ctx.error.code == "USER_ALREADY_EXISTS") {
+            toast.error("E-mail já cadastrado.");
+          }
+
+          toast.error("Erro ao criar a conta.");
         },
       },
     );
