ef1a4629733a25fd262a50b109617401f3cc745b
NathanManhkeBNUSC <nathanmanhke@gmail.com>
2025-08-21 15:51:13 -0300

    feat: implementar autenticação e redirecionamento na página de autenticação e dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,25 @@
-const DashboardPage = () => {
+import { auth } from "@/lib/auth";
+import { authClient } from "@/lib/auth-client";
+
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import SingOutButton from "./components/sign-out-button";
+
+const DashboardPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
   return (
     <div>
       <h1>DashboardPage</h1>
+      <h1>{session?.user?.name}</h1>
+      <h1>{session?.user?.email}</h1>
+      <SingOutButton />
     </div>
   );
 };
